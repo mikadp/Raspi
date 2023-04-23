e5ef1076e98b06470ace3d7201a4f3949e77913e
mikadp <mikasahlberg@outlook.com>
2023-04-23 20:10:14 +0300

    db connection and phonenum logic

--- database.go
+++ database.go
@@ -8,18 +8,41 @@ import (
 	"os"
 )
 
-func dbConnection() (*sql.DB, error) {
+var db *sql.DB
+
+func setupDatabaseConnection() {
 	// Read db username etc from .env
 	user := os.Getenv("MYSQL_USER")
 	password := os.Getenv("MYSQL_PASSWORD")
 	name := os.Getenv("MYSQL_DATABASE")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("ENV MYSQL_PORT")
 
 	// Create the database connection
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", user, password, name))
+	var err error
+	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, name))
 	if err != nil {
 		log.Fatalf("Cannot create the connection: %s", err)
+	}
+}
+
+func getPhoneNumbers() ([]string, error) {
+	// Query the phonenumbers table to retrieve all phone numbers
+	rows, err := db.Query("SELECT number FROM phonenumbers")
+	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
-	return db, nil
+	// Iterate over the rows and retrieve the phone numbers
+	var phoneNumbers []string
+	for rows.Next() {
+		var phoneNumber string
+		if err := rows.Scan(&phoneNumber); err != nil {
+			return nil, err
+		}
+		phoneNumbers = append(phoneNumbers, phoneNumber)
+	}
+
+	return phoneNumbers, nil
 }
